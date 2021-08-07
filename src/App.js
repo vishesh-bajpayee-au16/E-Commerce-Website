d213d5f6096100947103e4bcc82b2d0dd608bdad
Vishesh Bajpayee <vishesh@pop-os.localdomain>
2021-08-07 10:52:34 +0530

    adding header compoonent and links to home

--- src/App.js
+++ src/App.js
@@ -3,18 +3,13 @@ import HomePage from "./pages/homePage";
 import ShopPage from "./pages/shopPage";
 import { Route, Switch, Link } from "react-router-dom";
 import "./app.css";
-
-const hatsPage = (props) => {
-  console.log(props);
-  return <h1>HATS PAGE</h1>;
-};
+import Header from "./components/header";
 
 const App = () => {
   return (
     <div className="App">
-      {/* <HomePage /> */}
-      <Link to="/">HOME</Link>
-      <Link to="/shop">SHOP</Link>
+      <Header />
+
       <Switch>
         <Route path="/" exact={true} component={HomePage} />
         <Route path="/shop" exact={true} component={ShopPage} />
